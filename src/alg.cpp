42e2f544b96df9fcdb4b433659016122b246b70b
smirnovaalyona <99326020+smirnovaalyona@users.noreply.github.com>
2022-06-10 15:07:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,1 @@
 // Copyright 2021 NNTU-CS
-int begin, end;
-  int count;
-  int stepBack(int index) {
-    int result = —index;
-    int result = --index;
-    if (result < 0)
-      result += size + 1;
-    return result;
