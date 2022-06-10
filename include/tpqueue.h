e3dc34f5fe180d6b5572b6ea76f47c96f6282d11
smirnovaalyona <99326020+smirnovaalyona@users.noreply.github.com>
2022-06-10 14:49:04 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,8 +4,66 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
+   private:
+  struct ITEM {
+  T data;
+  ITEM* next;
+  ITEM* prev;
+  };
+  ITEM* head;
+  ITEM* tail;
+  TPQueue::ITEM* create(const T& data, ITEM*prev) {
+  ITEM* item = new ITEM;
+  item->data = data;
+  item->next = nullptr;
+  item->prev = prev;
+  return item;
+  }
+   public:
+  TPQueue() :head(nullptr), tail(nullptr) {}
+  ~TPQueue() {
+  while (head)
+  pop();
+  }
+  void push(const T& data) {
+  if (tail && head) {
+  ITEM* current = tail;
+  while (current && data.prior > (current->data).prior) {
+  current = current->prev;
+  }
+  if (current) {
+  ITEM* temp = current->next;
+  current->next = create(data, current);
+  current = current->next;
+  current->next = temp;
+  if (temp)
+  temp->prev = current;
+  else
+  tail = current;
+  }
+  else {
+  current = create(data, nullptr);
+  current->next = head;
+  head->prev = current;
+  head = current;
+  }
+  } 
+  else {
+  head = create(data, nullptr);
+  tail = head;
+  }
+  }
+  T pop() {
+  assert(head);
+  ITEM* temp = head->next;
+  T data = head->data;
+  if (temp)
+  temp->prev = nullptr;
+  delete head;
+  head = temp;
+  return data;
+  }
+  };
 
 struct SYM {
   char ch;
